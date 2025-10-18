876bc866919d128d1331af24e327738354381a80
VictorAkubo <138879202+VictorAkubo@users.noreply.github.com>
2025-10-18 16:34:24 +0100

    Update socket URL in Context.js

--- client/src/Context.js
+++ client/src/Context.js
@@ -5,7 +5,7 @@ import Peer from 'simple-peer';
 const SocketContext = createContext();
 
 //const socket = io('http://localhost:5000');
-const socket = io("https://watch-two-orpin.vercel.app");
+const socket = io("https://watch-j3rf.onrender.com/");
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
